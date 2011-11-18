244bf66f7f32a5101b162132e09a611585358aa3
Machiavelli <machiavelli.trinity@gmail.com>
2011-11-18 12:14:21 +0100

    Yell (Y) and Say (S) keybinds

---
+++ CLIClient/UI/CommandLine/KeyBindHandlers/Yell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Client.Chat.Definitions;
+using Client.World;
+using Client.World.Definitions;
+using Client.World.Network;
+
+namespace Client.UI.CommandLine
+{
+    public partial class CommandLineUI
+    {
+        [KeyBind(ConsoleKey.Y)]
+        public void DoYellChat()
+        {
+            Log("Yell: ");
+            var message = Game.UI.ReadLine();
+
+            var response = new OutPacket(WorldCommand.CMSG_MESSAGECHAT);
+
+            response.Write((uint)ChatMessageType.Yell);
+            var race = Game.World.SelectedCharacter.Race;
+            var language = race.IsHorde() ? Language.Orcish : Language.Common;
+            response.Write((uint)language);
+            response.Write(message.ToCString());
+            Game.SendPacket(response);
+        }
+    }
+}

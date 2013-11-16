b288c374ee696b92fe4eb485282d0f0cadbf36f8
jackpoz <giacomopoz@gmail.com>
2013-11-16 17:38:56 +0100

    Add missing file

---
+++ BotFarm/BotInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotFarm
+{
+    public class BotInfo
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public BotInfo() { }
+
+        public BotInfo(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+    }
+}

2a049432303979e930288ee84372b74774846892
jackpoz <giacomopoz@gmail.com>
2014-03-11 21:30:49 +0100

    Add missing file

---
+++ BotFarm/BotGame.cs
@@ -0,0 +1,31 @@
+﻿using Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotFarm
+{
+    class BotGame : AutomatedGame
+    {
+        public bool SettingUp
+        {
+            get;
+            set;
+        }
+
+        public BotGame(string hostname, int port, string username, string password, int realmId, int character)
+            : base(hostname, port, username, password, realmId, character)
+        { }
+
+        public override void NoCharactersFound()
+        {
+            if (!SettingUp)
+            {
+                Log("Removing current bot because there are no characters");
+                BotFactory.Instance.RemoveBot(this);
+            }
+        }
+    }
+}

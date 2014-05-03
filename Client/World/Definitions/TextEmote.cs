cd881955e83dfda62a777ad542aa27040d11206a
jackpoz <giacomopoz@gmail.com>
2014-05-03 19:03:15 +0200

    Add missing file

---
+++ Client/World/Definitions/TextEmote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.World.Definitions
+{
+    public enum TextEmote : uint
+    {
+        Yawn = 106
+    }
+}

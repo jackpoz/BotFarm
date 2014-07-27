41f4cbc2f45288c0710283ca69eb2e8ee743b353
jackpoz <giacomopoz@gmail.com>
2014-07-27 16:54:37 +0200

    Update Auth result codes

--- Client/World/Network/AuthResponse.cs
+++ Client/World/Network/AuthResponse.cs
@@ -7,8 +7,28 @@ namespace Client.World.Network
 {
     enum CommandDetail : byte
     {
-        AuthSuccess = 0x0C,
-        AuthQueue,
-        AuthFailure
+        AUTH_OK = 12,
+        AUTH_FAILED = 13,
+        AUTH_REJECT = 14,
+        AUTH_BAD_SERVER_PROOF = 15,
+        AUTH_UNAVAILABLE = 16,
+        AUTH_SYSTEM_ERROR = 17,
+        AUTH_BILLING_ERROR = 18,
+        AUTH_BILLING_EXPIRED = 19,
+        AUTH_VERSION_MISMATCH = 20,
+        AUTH_UNKNOWN_ACCOUNT = 21,
+        AUTH_INCORRECT_PASSWORD = 22,
+        AUTH_SESSION_EXPIRED = 23,
+        AUTH_SERVER_SHUTTING_DOWN = 24,
+        AUTH_ALREADY_LOGGING_IN = 25,
+        AUTH_LOGIN_SERVER_NOT_FOUND = 26,
+        AUTH_WAIT_QUEUE = 27,
+        AUTH_BANNED = 28,
+        AUTH_ALREADY_ONLINE = 29,
+        AUTH_NO_TIME = 30,
+        AUTH_DB_BUSY = 31,
+        AUTH_SUSPENDED = 32,
+        AUTH_PARENTAL_CONTROL = 33,
+        AUTH_LOCKED_ENFORCED = 34,
     }
 }

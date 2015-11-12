c9fd06b362256ab44addfd84b095302c8e9b3952
jackpoz <giacomopoz@gmail.com>
2015-11-12 22:43:50 +0100

    Add new BatchQueue<T> class as Multiple-writers, single-reader queue returning results in batches ignoring any further element queued till next batch

---
+++ Client/BatchQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client
+{
+    /// <summary>
+    /// Multiple-writers, single-reader queue returning results in batches ignoring any further element queued till next batch
+    /// </summary>
+    public class BatchQueue<T>
+    {
+        ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+
+        public void Enqueue(T item)
+        {
+            queue.Enqueue(item);
+        }
+
+        /// <summary>
+        /// Returns the content of the queue at the time it is called, ignoring any any further element queued till next call
+        /// </summary>
+        public IEnumerable<T> BatchDequeue()
+        {
+            T item;
+            for (int currentCount = queue.Count; currentCount > 0; currentCount--)
+                if (queue.TryDequeue(out item))
+                    yield return item;
+        }
+    }
+}

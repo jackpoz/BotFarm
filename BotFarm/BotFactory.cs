1471af4c7fd6025be6eda2ef6469195e3509eb26
jackpoz <giacomopoz@gmail.com>
2019-08-10 16:20:52 +0200

    Implement config option CreateAccountOnly

--- BotFarm/BotFactory.cs
+++ BotFarm/BotFactory.cs
@@ -109,7 +109,7 @@ namespace BotFarm
             factoryGame.Start();
         }
 
-        public BotGame CreateBot()
+        public BotGame CreateBot(bool startBot)
         {
             Log("Creating new bot");
 
@@ -140,7 +140,8 @@ namespace BotFarm
                                                 0,
                                                 botBehavior);
             game.SettingUp = true;
-            game.Start();
+            if (startBot)
+                game.Start();
             botInfos.Add(new BotInfo(username, password, botBehavior.Name));
 
             return game;
@@ -196,7 +197,7 @@ namespace BotFarm
             {
                 try
                 {
-                    var bot = CreateBot();
+                    var bot = CreateBot(!Settings.Default.CreateAccountOnly);
                     lock (bots)
                     {
                         bots.Add(bot);

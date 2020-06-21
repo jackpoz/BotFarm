6d183f8df37285849b1a61790874b09ee2c74f8a
jackpoz <giacomopoz@gmail.com>
2020-06-21 12:33:42 +0200

    Move helper functions to AutomatedGame

--- TrinityCore UnitTests/Helpers/Helper.cs
+++ TrinityCore UnitTests/Helpers/Helper.cs
@@ -9,24 +9,5 @@ namespace TrinityCore_UnitTests.Helpers
 {
     static class Helper
     {
-        public static async Task ScheduleActionAndWait(this AutomatedGame game, Action action, int waitMilliseconds = 0)
-        {
-            await game.ScheduleActionAndWait(action, DateTime.Now, waitMilliseconds);
-        }
-
-        public static async Task ScheduleActionAndWait(this AutomatedGame game, Action action, DateTime time, int waitMilliseconds = 0)
-        {
-            var semaphore = new SemaphoreSlim(0);
-            game.ScheduleAction(() =>
-            {
-                action();
-                semaphore.Release();
-            }, time);
-
-            await semaphore.WaitAsync();
-
-            if (waitMilliseconds > 0)
-                await Task.Delay(waitMilliseconds);
-        }
     }
 }
